10d21d3b77180f54cf5366c351a4f07ae9caf13e
Nikita <nikitakhapare53@gmail.com>
2022-03-31 19:26:35 +0530

    [Nikita]Add:Program to calculate Employee Wage

--- Employee.py
+++ Employee.py
@@ -2,22 +2,27 @@
 @Author: Nikita
 @Date: 2022-03-29 20: 56: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-03-31 19: 17: 00
-@Title: Calculating Daily Wage
+@Last Modified time: 2022-03-31 19: 26: 00
+@Title: Calculating Employee Wage
 '''
 
 import random
 
 print("Welcome to Employee Wage Computation Programs")
 fullTime=1
+partTime=2
 wagePerHr=20
 employeeWage=0
 Present=1
 attendance=random.randint(0,1)
 
-if attendance==1:   
+if attendance==Present:   
     print("Employee is Present")
-    empHr=8
+    empChack=random.randint(0,1)
+    if empChack==fullTime:
+        empHr=8
+    elif empChack==partTime:
+        empHr=4   
     employeeWage=empHr*wagePerHr
     print("Daily Employee Wage for Employee is ",+employeeWage)
 
