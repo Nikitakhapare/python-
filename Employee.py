ce9629b436635a585639e1140b7aae156b837285
Nikita <nikitakhapare53@gmail.com>
2022-04-01 00:55:00 +0530

    [nikita]Refactor:Changing Naming Convention of Program

--- Employee.py
+++ Employee.py
@@ -1,12 +1,14 @@
 '''
 @Author: Nikita
-@Date: 2022-03-29 20: 56: 00
+@Date: 2022-03-29 20: 51: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-03-31 : 20: 14
-@Title: Calculating Employee Wage Using Case Statement
+@Last Modified time: 2022-04-01 : 12: 51
+@Title: Calculating  Wage till condition of working hour or days reached
 '''
 
 import random
+
+
 def workingHr(check):
     """
         Description:
@@ -16,8 +18,9 @@ def workingHr(check):
         Return:   
             returning the check variable
     """
-    switcher={ 1: 8, 2: 4}
+    switcher = {1: 8, 2: 4}
     return switcher[check]
+
 
 def attendance(checkAttendance):
     """
@@ -28,32 +31,35 @@ def attendance(checkAttendance):
         Return:   
             returning the check variable
     """
-    switcher={0:0,1:1}
+    switcher = {0: 0, 1: 1}
     return switcher[checkAttendance]
-    
-wagePerHour=20
-fullTime=1
-partTime=0
-empWorkingHr=0
-totalWorkingDays=0
-maxWorkingDays=20
-totalEmpHours=0
-maxWorkingHr=100
 
-#while(totalWorkingDays<maxWorkingDays)and(totalEmpHours<maxWorkingHr):
-for i in range(21):
-    checkAttendance=random.randint(0,1)
-    attendancechake=attendance(checkAttendance)
-    if attendancechake==1:   
-        check = random.randint(1,2)
-        empWorkingHr=workingHr(check)
-        totalEmpHours+=empWorkingHr 
-        print(i,totalEmpHours)
 
-    if totalEmpHours >= 100:
-        break  
-  
-totalWage=(wagePerHour*totalEmpHours)
-print("Employee Working Days is ", i)
-print("Employee Working Hour is",totalEmpHours)
-print("Monthly Employee Wage= ",totalWage)
+wagePerHour = 20
+fullTime = 1
+partTime = 0
+empWorkingHr = 0
+totalWorkingDays = 0
+maxWorkingDays = 20
+totalEmpHours = 0
+maxWorkingHr = 100
+
+def calculate_wage():
+
+    for i in range(21):
+        totalEmpHours = 0
+        checkAttendance = random.randint(0, 1)
+        attendancechake = attendance(checkAttendance)
+        if attendancechake == 1:
+            check = random.randint(1, 2)
+            empWorkingHr = workingHr(check)
+            dailyWa
+            totalEmpHours += empWorkingHr
+
+        if totalEmpHours >= 100:
+            break
+
+    totalWage = (wagePerHour*totalEmpHours)
+    print("Monthly Employee Wage= ", totalWage)
+
+calculate_wage()
