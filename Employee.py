51e09c24b01430a280c1be20b074d0691fc34c8d
Nikita <nikitakhapare53@gmail.com>
2022-04-01 08:54:06 +0530

    [nikita]Add:Program to store Day along with Daily Wage

--- Employee.py
+++ Employee.py
@@ -3,11 +3,12 @@
 @Date: 2022-03-29 20: 51: 00
 @Last Modified by: Nikita
 @Last Modified time: 2022-04-01 : 8: 32
-@Title: uc8 Store Daily Wage Along with Total Wage
+@Title: uc9 Store Day and Daily Wage Along with Total Wage
 '''
 
 import random
 from re import A
+
 
 def workingHr(check):
     """
@@ -44,8 +45,8 @@ maxWorkingDays = 20
 totalEmpHours = 0
 maxWorkingHr = 100
 
-def calculateWage():
 
+def calculateWage():
     """
         Description:
             Function to Calculate Hour
@@ -54,31 +55,33 @@ def calculateWage():
         Return:
             returning the total working Hour
     """
-    list=[]
-    a=0
-    totalEmpHours=0
-    totalDays=0
-    while a<20:
+    list = []
+    daylist = []
+    a = 0
+    totalEmpHours = 0
+    totalDays = 0
+    while a < 20:
         checkAttendance = random.randint(0, 1)
         attendancechake = attendance(checkAttendance)
         if attendancechake == 1:
             check = random.randint(1, 2)
             empWorkingHr = workingHr(check)
-            dailyWage=wagePerHour*empWorkingHr
+            dailyWage = wagePerHour*empWorkingHr
             list.append(dailyWage)
             totalEmpHours += empWorkingHr
+            daylist.append(a)
 
         else:
-            a-=1
+            a -= 1
 
         if totalEmpHours >= 100:
             break
-        a+=1
-        
+        a += 1
 
     totalWage = (wagePerHour*totalEmpHours)
-    print("Total Working Days in month ",a+1)
-    print("Total Working Hour is ",totalEmpHours)
-    print("Daily Wage of Employee is ",list)
+    print("Total Working Days in month ", a+1)
+    print("Total Working Hour is ", totalEmpHours)
+    print("Daily Wage of Employee is ", list)
+    print("Day list ", daylist)
 
 calculateWage()
