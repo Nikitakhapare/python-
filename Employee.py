0baa31195955dbddb012cf9178c6be9d8c5e8eeb
Nikita <nikitakhapare53@gmail.com>
2022-04-01 08:37:53 +0530

    [nikita]Add:Program to store Daily Wage Along With Total Wage

--- Employee.py
+++ Employee.py
@@ -2,11 +2,12 @@
 @Author: Nikita
 @Date: 2022-03-29 20: 51: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-04-01 : 7: 38
-@Title: Function To Calculate Total Working Hour
+@Last Modified time: 2022-04-01 : 8: 32
+@Title: uc8 Store Daily Wage Along with Total Wage
 '''
 
 import random
+from re import A
 
 def workingHr(check):
     """
@@ -53,21 +54,31 @@ def calculateWage():
         Return:
             returning the total working Hour
     """
+    list=[]
+    a=0
     totalEmpHours=0
-    for i in range(20):
+    totalDays=0
+    while a<20:
         checkAttendance = random.randint(0, 1)
         attendancechake = attendance(checkAttendance)
         if attendancechake == 1:
             check = random.randint(1, 2)
             empWorkingHr = workingHr(check)
+            dailyWage=wagePerHour*empWorkingHr
+            list.append(dailyWage)
             totalEmpHours += empWorkingHr
-            
+
+        else:
+            a-=1
 
         if totalEmpHours >= 100:
             break
+        a+=1
+        
 
     totalWage = (wagePerHour*totalEmpHours)
-    print("Total Working Days in month ", i+1)
+    print("Total Working Days in month ",a+1)
     print("Total Working Hour is ",totalEmpHours)
+    print("Daily Wage of Employee is ",list)
 
 calculateWage()
